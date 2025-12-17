2c197bfcdd1c364d2e1977598dd7a3670be24cc4
hmirodski <48515132+hmirodski@users.noreply.github.com>
2025-12-17 11:23:04 +0100

    feat: query 4 -tienda

--- queries/queries.sql
+++ queries/queries.sql
@@ -8,7 +8,8 @@ SELECT nombre, precio FROM producto;
 SELECT * FROM producto;
 
 -- 4. Llista el nom dels productes, el preu en euros (precio_eur) i el preu en dòlars estatunidencs (precio_usd) amb un tipus de canvi de 1 € = 1 $.
-
+SELECT nombre, precio AS precio_eur, precio AS precio_usd
+FROM producto;
 
 -- 5. Llista el nom dels productes, el preu en euros i el preu en dòlars estatunidencs (amb un tipus de canvi de 1 € = 1,1 $ i arrodonint el resultat a dues xifres decimals). Utilitza els següents àlies per a les columnes: nom del producte, euros, dòlars.
 
